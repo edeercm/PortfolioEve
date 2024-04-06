74510c8a0aaa81ea80587927e06a3dd26a5b2b4d
Eder <132967872+edeercm@users.noreply.github.com>
2024-04-06 04:05:46 -0600

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,13 @@ import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import Eve from '../../assets/images/EVE.jpg'
 
 const Section = styled.section`
-    position: relative;
-    width: 100%;
-    height: 97.5vh;
-    display: flex;
-    text-align: end;
-    align-items: center;
-    background-color: var(--principal-color);
+  position: relative;
+  width: 100%;
+  height: 97.5vh;
+  display: flex;
+  text-align: end;
+  align-items: center;
+  background-color: var(--principal-color);
 
   @media (max-width: 575.97px) {
   }
