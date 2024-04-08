12936b4098ff155940e5f7a6c0e0191e9bb3fdd6
Eder <132967872+edeercm@users.noreply.github.com>
2024-04-08 01:10:22 -0600

    Update resume section

--- src/pages/Resume/ResSecFive.jsx
+++ src/pages/Resume/ResSecFive.jsx
@@ -1,67 +1,46 @@
-import React from 'react'
-import styled from 'styled-components'
-import { LblOne, LblThree } from './ResGlobalSty'
-import autocad from '../../assets/images/icons/autocad.png'
-import sketchup from '../../assets/images/icons/sketchup.png'
-import vray from '../../assets/images/icons/vray.png'
-import illustrator from '../../assets/images/icons/illustrator.png'
-import photoshop from '../../assets/images/icons/photoshop.png'
-import office from '../../assets/images/icons/office.png'
-import revit from '../../assets/images/icons/revit.png'
+import React from 'react';
+import styled from 'styled-components';
+import { LblOne, LblThree } from './ResGlobalSty';
+import autocad from '../../assets/images/icons/autocad.png';
+import sketchup from '../../assets/images/icons/sketchup.png';
+import vray from '../../assets/images/icons/vray.png';
+import illustrator from '../../assets/images/icons/illustrator.png';
+import photoshop from '../../assets/images/icons/photoshop.png';
+import office from '../../assets/images/icons/office.png';
+import revit from '../../assets/images/icons/revit.png';
 
 const Icon = styled.img`
   width: 35%;
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-  }
 
   @media (max-width: 575.97px) {
     width: 40%;
   }
 `;
 
+const softwareList = [
+  { name: 'AutoCad', icon: autocad },
+  { name: 'SketchUp', icon: sketchup },
+  { name: 'Vray', icon: vray },
+  { name: 'Illustrator', icon: illustrator },
+  { name: 'Photoshop', icon: photoshop },
+  { name: 'Office', icon: office },
+  { name: 'Revit', icon: revit },
+];
+
 const ResSecFive = () => {
-  return <>
+  return (
     <div className='d-flex flex-column gap-3'>
       <LblOne>Software</LblOne>
-      <div className='d-flex flex-row'>
-        <div className='d-flex flex-column gap-3'>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={autocad} alt="AutoCad-icon" />
-            <LblThree>AutoCad</LblThree>
+      <div className='row'>
+        {softwareList.map((software) => (
+          <div className='col-4 d-flex flex-column align-items-center mb-3'>
+            <Icon src={software.icon} alt={`${software.name}-icon`} />
+            <LblThree>{software.name}</LblThree>
           </div>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={illustrator} alt="Illustrator-icon" />
-            <LblThree>Illustrator</LblThree>
-          </div>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={revit} alt="Revit-icon" />
-            <LblThree>Revit</LblThree>
-          </div>
-        </div>
-        <div className='d-flex flex-column gap-3'>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={sketchup} alt="SketchUp-icon" />
-            <LblThree>SketchUp</LblThree>
-          </div>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={photoshop} alt="Photoshop-icon" />
-            <LblThree>Photoshop</LblThree>
-          </div>
-        </div>
-        <div className='d-flex flex-column gap-3'>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={vray} alt="Vray-icon" />
-            <LblThree>Vray</LblThree>
-          </div>
-          <div className='d-flex flex-column align-items-center'>
-            <Icon src={office} alt="Office-icon" />
-            <LblThree>Office</LblThree>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  </>
-}
+  );
+};
 
-export default ResSecFive
+export default ResSecFive;
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -4,12 +4,12 @@ import ResSecThree from './ResSecThree'
 import ResSecFour from './ResSecFour'
 import ResSecFive from './ResSecFive'
 
-
 const Section = styled.section`
   width: 100%;
   height: 97.5vh;
   display: flex;
   align-items: center;
+  justify-content: center;
 
   @media (min-width: 575.98px) and (max-width: 991.98px) {
   }
@@ -66,124 +66,12 @@ const Number = styled.h5`
   }
 `;
 
-const Contonedotone = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 0.5rem;
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-    margin-top: 1.25rem;
-  }
-`;
-
-const Label = styled.h5`
-    color: var( --secondary-color);
-    font-weight: bold;
-    font-size: 1.275rem;
-    letter-spacing: 0.1rem;
-    text-transform: uppercase;
-
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-    /* font-size: 1.5rem; */
-
-  }
-`;
-
-const Labelthree = styled.span`
-    font-style: oblique;
-    color: var(--quaternary-color);
-
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-    /* font-size: 1.15rem; */
-
-  }
-`;
-
-const Conthree = styled.div`
-  margin-top: 0.5rem;
-  display: flex;
-  flex-direction: row;
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-  @media (max-width: 575.97px) {
-    margin-top: 1rem;
-  }
-`;
-
-const Contfour = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.875rem;
-
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-
-  }
-`;
-
-const Icon = styled.img`
-  width: 35%;
-  object-fit: contain;
-  object-position: center;
-
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-    width: 40%;
-  }
-`;
-
-const Contfive = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-
-  @media (min-width: 575.98px) and (max-width: 991.98px) {
-
-  }
-
-
-  @media (max-width: 575.97px) {
-
-  }
-`;
-
 const Resume = () => {
   return <>
     <Section>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 mb-4">
+      <div className="container mt-4">
+        <div className="row gap-5">
+          <div className="col-12">
             <div className="row">
               <div className="col-12 col-md-6">
                 <Persname>Evelin<br></br>Alvarado</Persname>
@@ -206,45 +94,6 @@ const Resume = () => {
               </div>
               <div className="col-12 col-md-4">
                 <ResSecFive />
-                {/* <Contonedotone>
-                  <Label>Software</Label>
-                  <Conthree>
-                    <Contfour>
-                      <Contfive>
-                        <Icon src={autocad} alt="AutoCad-icon" />
-                        <Labelthree>AutoCad</Labelthree>
-                      </Contfive>
-                      <Contfive>
-                        <Icon src={illustrator} alt="Illustrator-icon" />
-                        <Labelthree>Illustrator</Labelthree>
-                      </Contfive>
-                      <Contfive>
-                        <Icon src={revit} alt="Revit-icon" />
-                        <Labelthree>Revit</Labelthree>
-                      </Contfive>
-                    </Contfour>
-                    <Contfour>
-                      <Contfive>
-                        <Icon src={sketchup} alt="SketchUp-icon" />
-                        <Labelthree>SketchUp</Labelthree>
-                      </Contfive>
-                      <Contfive>
-                        <Icon src={photoshop} alt="Photoshop-icon" />
-                        <Labelthree>Photoshop</Labelthree>
-                      </Contfive>
-                    </Contfour>
-                    <Contfour>
-                      <Contfive>
-                        <Icon src={vray} alt="Vray-icon" />
-                        <Labelthree>Vray</Labelthree>
-                      </Contfive>
-                      <Contfive>
-                        <Icon src={office} alt="Office-icon" />
-                        <Labelthree>Office</Labelthree>
-                      </Contfive>
-                    </Contfour>
-                  </Conthree>
-                </Contonedotone> */}
               </div>
             </div>
           </div>
